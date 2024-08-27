9d783596e2e53d8a446e122263ea1bb907d199db
Soumyadeep Ghosh <sam94339@gmail.com>
2024-08-27 20:20:33 +0530

    Soumyadeep 27/08/2024

--- src/test/java/BasePackage/BaseClass.java
+++ src/test/java/BasePackage/BaseClass.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -42,8 +43,15 @@ public class BaseClass {
 		
 		if(browsername.contains("chrome")) {
 			
+			ChromeOptions option = new ChromeOptions();
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			
+			if(browsername.contains("headless")) {
+				
+				option.addArguments("headless");
+			}
+			
+			driver = new ChromeDriver(option);
 		}
 		
 		else if(browsername.contains("firefox")){
